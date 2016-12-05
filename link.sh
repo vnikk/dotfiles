d9f4400353098489bb64e067573f6118b7b0c90a
vnikk <vnikk>
2016-12-05 22:59:35 +0100

    get git-prompt

--- link.sh
+++ link.sh
@@ -2,6 +2,9 @@
 ln -s -f $PWD/bashrc ~/.bashrc
 ln -s -f $PWD/zshrc ~/.zshrc
 ln -s -f $PWD/vimrc ~/.vimrc
+
 for i in $(ls *.vim)
     do ln -s -f $PWD/$i ~/.vim/colors/$i
 done
+
+curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
