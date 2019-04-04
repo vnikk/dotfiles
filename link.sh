6b8778bd19aaf01c91722610bb4d64b71a341c8b
serge <serge@debian.serge>
2019-04-04 19:36:23 +0200

    add git alias

--- link.sh
+++ link.sh
@@ -16,6 +16,9 @@ function colors() {
 }
 
 function gitalias() {
+    git config --global user.name "vnikk"
+    git config --global user.email vnikk@github.com
+
     git config --global alias.xlog "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
     git config --global alias.lg1 "log --graph --pretty=format:\"%C(yellow)%h%Creset %ad  %s%C(cyan)%d%Creset %C(green)[%an]%Creset\" --date=short"
 }
